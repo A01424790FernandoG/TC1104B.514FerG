b0e51091ff891c61ac5672dc13287208402d4ed8
A01424790FernandoG <A01424790@itesm.mx>
2022-10-06 13:56:54 +0000

    practica 3

---
+++ tomay.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <ctype.h>
+
+
+int main(){
+    char c;
+    char may;
+    int n;
+    do{
+        n=read(STDIN_FILENO,&c,1);
+        may = toupper(c);
+        if(may == 'A'|| may=='E' || may=='I' || may== 'O' || may=='U' || may=='a'|| may=='e'|| may=='i'|| may=='o'|| may=='u'){
+            write(2, "La letra es vocal", 17);
+            write(STDOUT_FILENO,&may,1);
+        }
+    }while(n !=0);
+    return 0;
+}
