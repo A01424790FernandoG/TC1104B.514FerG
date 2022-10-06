62a06be5198d1a9f12581856893e699e8ee766fc
A01424790FernandoG <A01424790@itesm.mx>
2022-10-06 13:18:02 +0000

    Practica 2

---
+++ signal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void hola(int signalNum){
+    printf("Recibi la senial %d", signalNum);
+}
+
+int cond;
+void terminarWhile(int signalN){
+    printf("Terminando while");
+    cond = 0;
+}
+
+int main(){
+    signal(12,terminarWhile);
+    
+    //signal(2, hola);
+    cond = 1;
+    while(cond==1){
+        printf("Chanbeando \n");
+        sleep(1);
+    }
+    printf("Aqui nunca llega");
+    return 0;
+}
+
+//gcc -o signal signal.cpp
+//ps -fea
+//kill (senial) (numero de proceso de -fea)
+
+//kill -9 (numproceso) El numero 9 es para matar el proceso
+//tarrea romper ciclo con la senial 12
